365f43833d517b9a05b79f3e3f51921984f3f1a5
mghildiy <manish.c.ghildiyal@gmail.com>
2023-11-26 12:11:49 +0530

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Welcome to guessing game");
-    let width = message.chars().count();
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
     println!("Let's start the game");
 
-    let mut guess = String::new(); // a string variable whose value can be mutated
-    println!("Please guess the number");
-    io::stdin()
-        .read_line(&mut guess) // passing reference, that is mutable
-        .expect("Failed to read line");
+    let secretNumber = rand::thread_rng().gen_range(1..=100);
+    
+    loop {
 
-    println!("Your guess: {guess}");
+        let mut guess = String::new(); // a string variable whose value can be mutated
+        println!("Please guess the number");
+        io::stdin()
+            .read_line(&mut guess) // passing reference, that is mutable
+            .expect("Failed to read line");
+
+        // rust shadows variable defined earlier with same name
+        let guess: u32 = match guess.trim().parse() {
+            Ok(val) => val,
+            Err(_) => {
+                println!("You can enter a number only");
+                continue;
+            }
+        };
+
+        println!("Your guess: {guess}");
+
+        match guess.cmp(&secretNumber) {
+            Ordering::Less => println!("You guessed lower number"),
+            Ordering::Greater => println!("You guessed higher number"),
+            Ordering::Equal => {
+                println!("You are a champion");
+                break;
+            }
+        }
+    }
 }
