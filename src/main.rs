335e48fc5d7f5fbebff1a8268233d2dde5b5741e
mghildiy <manish.c.ghildiyal@gmail.com>
2023-12-02 08:26:54 +0530

    some basic experiments

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,18 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
+    // guessingGame();
+    //experiments();
+    //println!("{}", someRandomNumber(4));
+    controlFlow();
+}
+
+fn guessingGame() {
     println!("Let's start the game");
 
     let secretNumber = rand::thread_rng().gen_range(1..=100);
     
     loop {
-
         let mut guess = String::new(); // a string variable whose value can be mutated
         println!("Please guess the number");
         io::stdin()
@@ -35,4 +41,64 @@ fn main() {
             }
         }
     }
+}
+
+fn experiments() {
+    // default integer type is i32
+    let x = 5;
+    // shadowing
+    let x = "hello";
+    // a variable is immutable by default
+    let m = "immutable";
+    // mutable
+    let mut y = "mutable";
+    // constant
+    const SECONDS_IN_A_HOUR: u32 = 60 * 60 * 60;
+    let z: u32 = "7".parse().expect("Not a number!");
+    // default float type is f64
+    let a = 2.0;
+    let b: f32 = 2.0;
+    let c = true;
+    // tuple can contain multipke values of diff types
+    let d = (500, "hello", true);
+    // access tuple values with . or destructuring
+    let e = d.0;
+    let (_, f, g) = d;
+    println!("{e}, {f}, {g}");
+    // arrays contain multiple values of same type, they reside on stack
+    let days = ["Sunday", "Monday", "Tuesaday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let firstDay = days[0];
+    println!("{firstDay}");
+    
+}
+
+fn someRandomNumber(factor: u32) -> u32 {
+    rand::thread_rng().gen_range(1..=100) * factor
+}
+
+fn controlFlow() {
+    let x = 7;
+    let str = if x <= 7 {
+        "Less than or equal to 7"
+    } else {
+        "Greater than 7"
+    };
+    println!("{str}");
+
+    let x = 8;
+    let str = if x <= 7 {
+        println!("Less than or equal to 7")
+    } else {
+        println!("Greater than 7");
+    };
+
+    let mut counter = 0;
+    let y = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter
+        }
+    };
+    println!("Loop iterated {y} times");
+
 }
